c50591cb87b2a877d0ad374cc568d7ebc7ff9ebd
Dov peles <dovpeles@Dovs-Mac-mini.lan>
2023-11-21 14:55:03 -0500

    Initial commit

---
+++ pub.sh
@@ -0,0 +1,39 @@
+# git init
+# export PATH=/usr/local/share/npm/bin:$PATH
+
+git add .
+git commit -m "Initial commit"
+git push 
+# Creating a New Git Repository on a Hosting Service (e.g., GitHub, GitLab):
+# 1. Create a Repository on the Hosting Service:
+# For GitHub:
+
+# Go to GitHub and log in.
+# Click on the "New" button to create a new repository.
+# Follow the instructions to name your repository, add a description, choose visibility settings, etc.
+# Do not initialize the repository with a README, .gitignore, or license if you've already initialized your project locally.
+
+# 2. Connect the Local Repository to the Remote Repository:
+# If you've initialized a local repository and want to connect it to a remote repository on GitHub, GitLab, etc., follow these steps:
+
+
+#
+
+## git remote add origin <remote_repository_URL>
+## 👇️ install yarn cli globally
+# npm install -g yarn
+# heroku git:clone -a ches1 herochess1
+# Connected to  jookie/gaj4 by jookie jookie
+# heroku git:clone -a gaj4 hero_gaj4
+# Connected to  jookie/info_soft by jookie jookie
+# heroku git:clone -a info22 hero_info22
+# heroku git:clone -a live02 hero_live02
+# heroku git:clone -a oxy2 hero_oxy
+# http://localhost:9001/demos/video-conferencing.html
+# Unable to listen on port: 9001 
+# http://localhost:9001/ is already in use. Please kill below processes using "kill PID". 
+# ------------------------------
+# COMMAND  PID     USER   FD   TYPE             DEVICE SIZE/OFF NODE NAME
+# node    5926 MacAdmin   23u  IPv4 0x9c6759e5df48fca9      0t0  TCP *:e
+
+
